608814b7ff16c59674128931628ca904510f62ba
Arian <arian.kulmer@web.de>
2021-05-30 23:26:35 +0200

    sm6250-common: Include c2 manifest from media HAL
    Change-Id: I9ea0322af97284ef4b995df185d0c827285c7194

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
