5eb4a451cb21ab11877b23498505c9df0ba79845
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    sm6250-common: Drop support for Snapdragon Camera
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- common.mk
+++ common.mk
@@ -102,8 +102,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    libgui_vendor \
-    Snap
+    libgui_vendor 
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
