e3b47bfb98b471d2f894332cef74e1968a0faacd
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    sm6250-common: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,7 +83,6 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_ADDITIONAL_FLAGS += AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_CONFIG := cust_defconfig
