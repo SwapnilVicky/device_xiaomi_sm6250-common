299469acaf848eb92a77f16da7dc5aaead686e1a
Alexander Winkowski <dereference23@outlook.com>
2022-09-03 18:42:10 +0000

    sm6250-common: Remove android.hidl.base@1.0
    * Not referenced by any blobs.
    
    Change-Id: I8f0d4b65972ffc49d5c47fd23a6f3eb939f3a0db

--- common.mk
+++ common.mk
@@ -192,8 +192,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0.vendor \
     android.hidl.memory.block@1.0 \
