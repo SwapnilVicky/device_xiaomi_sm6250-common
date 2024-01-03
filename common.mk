65c24c0e25733b150c0480905fe775dee5111427
SwapnilVicky <swapnil.motoe@gmail.com>
2024-01-03 21:23:41 +0530

    sm6250-common: include missing separator

--- common.mk
+++ common.mk
@@ -83,7 +83,7 @@ PRODUCT_PACKAGES += \
     BluetoothQti
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
