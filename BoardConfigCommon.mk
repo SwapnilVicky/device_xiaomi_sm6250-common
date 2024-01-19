637148f927a5b126859d077b3dfc7e3e19f347e9
SwapnilVicky <swapnil.motoe@gmail.com>
2024-01-19 15:47:03 +0530

    Revert "sm6250-common: Switch to proton clang"
    This reverts commit 4cee2b3f3f3bb0689c706d6beb2ef83cbfbb389d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,8 +73,6 @@ BOARD_RAMDISK_USE_LZ4 := true
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_ADDITIONAL_FLAGS += AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_CONFIG := cust_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 
