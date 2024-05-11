2014f096100417191ba3c99bd4992499c41cda7a
SwapnilVicky <swapnil.motoe@gmail.com>
2024-05-11 18:29:54 +0530

    sm6250-common: Convert overlays to RROs -This commit is squashed with the following commits: * sm6250-common: overlay: Indicate device has a power button fingerprint sensor * sm6250-common: overlay: Add physical button/sensor locations * sm6250-common: overlay: Remove obsolete config_keyboardTapVibePattern * sm6250-common: overlay: Offload WM Shell to Another Thread * sm6250-common: Re-enable WiFi display overlays * sm6250-common: Unpin updatable-media from memory * sm6250-common: overlay: Only white led is present * sm6250-common: overlay: Adjust overlays for recently added light capability * sm6250-common: remove smart charging overlays * sm6250-common: overlay: CarrierConfig: update from sweetin V14.0.1.0.TKFINXM                 - from sweetin-user 13 TKQ1.221013.002 V14.0.1.0.TKFINXM release-keys * sm6250-common: CarrierConfigOverlay: Enable editable_enhanced_4g_lte_bool globally * sm6250-common: overlay: Migrate 'rounded_corner_content_padding' * sm6250-common: overlay: adjust status_bar padding * sm6250-common: overlay: adjust number of notification icons on status_bar * sm6250-common: overlay: Enable VoLTE/VoWIFI For All Indian Carriers * sm6250-common: overlay: Drop NotchBarKiller overlay * sm6250-common: overlay: Drop NoCutoutOverlay overlay
    Signed-off-by: SwapnilVicky <swapnil.motoe@gmail.com>

--- common.mk
+++ common.mk
@@ -303,16 +303,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# Notch Cutout
-PRODUCT_PACKAGES += \
-    miatollNoCutoutOverlay \
-    NotchBarKiller
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    MiatollCarrierConfigOverlay \
+    MiatollFrameworks \
+    MiatollSettingsOverlay \
+    MiatollSystemUI \
+    MiatollTelephonyResCommon \
+    MiatollWifiOverlay 
+    
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
@@ -396,10 +398,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # TimeKeep
 PRODUCT_PACKAGES += \
     TimeKeep
@@ -456,7 +454,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
