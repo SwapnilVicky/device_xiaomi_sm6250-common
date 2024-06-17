acac2fd6edf59ca42dc966908ac7385bf61d6278
Bruno Martins <bgcngm@gmail.com>
2024-06-17 21:59:49 +0800

    sm6250-common: Include common xiaomi FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,6 +85,7 @@ BOARD_KERNEL_CMDLINE += kpti=off
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
 	$(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
