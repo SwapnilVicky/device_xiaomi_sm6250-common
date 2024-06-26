16563af57ee2589f7fa13b6e0346fbbb4a8c7282
COSMIC <rayavanindra@gmail.com>
2024-06-26 13:52:50 +0530

    sm6250-common: Switch to QTI Thermal AIDL HAL
    Change-Id: I42f90059c8e2fd84885c7f3ddf9381ee7c9a9f3a
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- atoll.mk
+++ atoll.mk
@@ -287,11 +287,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel \
-    thermal_symlinks
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal-service.qti
 
 # TimeKeep
 PRODUCT_PACKAGES += \
