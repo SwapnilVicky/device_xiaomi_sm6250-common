1243f081602ffa68a11f9e1fa3186df8839fcf8b
SwapnilVicky <swapnil.motoe@gmail.com>
2024-06-27 19:33:26 +0530

    sm6250-common: unset SELINUX_IGNORE_NEVERALLOWS := true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,7 +178,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
