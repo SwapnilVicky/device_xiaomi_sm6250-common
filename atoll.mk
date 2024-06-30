91730fe1e13d3712c86028628143a0589ca22e6c
0xCAFEBABE <me.cafebabe@gmail.com>
2024-06-30 20:42:43 +0800

    sm6250-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- atoll.mk
+++ atoll.mk
@@ -160,6 +160,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     NcmTetheringOverlay \
+    SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
