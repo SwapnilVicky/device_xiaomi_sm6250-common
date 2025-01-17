dc37b58efce315c336a95c3a57d9a402ff9c5359
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    sm6250-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- common.mk
+++ common.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
